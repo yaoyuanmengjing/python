7dab0b981f506e00c02fda1825e12bde2ccdd742
liuyuzhou <liuyuzhou@21eline.com>
2017-08-07 14:44:24 +0800

    132

--- Employee2.py
+++ Employee2.py
@@ -2,20 +2,19 @@
 # -*- coding: UTF-8 -*-
 
 class Employee:
-    '所有员工的基类'
+    #'所有员工的基类'
     empCount = 0
-    
-    
-def __init__(self,name,salary):
-    self.name   = name
-    self.salary = salary
-    Employee.empCount+=1
-    
-def dsiplayCount(self):
-    print "总数是%d" % Employee.empCount
-    
-def  displayEmployee(self):
-    print "name:",self.name, ",salary:",self.salary
+      
+      
+      
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+        Employee.empCount += 1   
+    def displayCount(self):
+        print "Total Employee %d" % Employee.empCount  
+    def displayEmployee(self):
+        print "Name : ", self.name,  ", Salary: ", self.salary
     
     
     
